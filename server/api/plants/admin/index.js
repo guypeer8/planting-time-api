deda065417793e6187e62e1ee772d37386673a9d
guypeer8 <guypeer8@gmail.com>
2021-02-22 15:51:14 +0200

    create

--- server/api/plants/admin/index.js
+++ server/api/plants/admin/index.js
@@ -4,6 +4,26 @@ const router = require('express').Router();
 const PlantModel = require('../../../../models/plant.model');
 
 /**
+ * /api/plants/:plant_id --> create/update plant
+ */
+router.put('/:plant_id', async (req, res) => {
+    try {
+        const { plant_id: _id } = req.params;
+        if (!_id) {
+            const plantRecord = new PlantModel(req.body);
+            const plant = await plantRecord.save();
+            return res.json({ status: 'success', payload: plant });
+        }
+        const { ok, n, nModified } = await PlantModel.update({ _id }, { $set: req.body });
+        const plant = await PlantModel.findOne({ _id });
+        if (!ok || n !== 1 || nModified !== 1) { throw new Error('Update failed!'); }
+        res.json({ status: 'success', payload: plant });
+    } catch(e) {
+        res.json({ status: 'error', error: e });
+    }
+});
+
+/**
  * /api/plants/:plant_id --> update plant
  */
 router.put('/:plant_id', async (req, res) => {
